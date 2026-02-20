6168c044db0436c8e61e8d101d400172634fc012
Vincevdb1 <vincent.vandebosch@icloud.com>
2026-02-20 17:30:42 +0100

    chore: Setup

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-use ratatui::{DefaultTerminal, Frame};
+use color_eyre::eyre::Result;
+use ratatui::{
+    crossterm::event::{self, Event},
+    widgets::{Paragraph, Widget},
+    DefaultTerminal, Frame
+};
 
-fn main() -> color_eyre::Result<()> {
+fn main() -> Result<()> {
     color_eyre::install()?;
-    ratatui::run(app)?;
+
+    let terminal = ratatui::init();
+    let result = run(terminal);
+
+    ratatui::restore();
+
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        // Rendering
+        terminal.draw(render)?;
+
+        // Input Handling
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Char('q') => break,
+                _ => {}
+            }
+        }
+    }
+
     Ok(())
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
-        }
-    }
+fn render(frame: &mut Frame) {
+    Paragraph::new("Test").render(frame.area(), frame.buffer_mut());
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
-}
