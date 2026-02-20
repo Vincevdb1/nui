0d7defbd55fb89e1fba30483ea515ed94f7f4162
Vincevdb1 <vincent.vandebosch@icloud.com>
2026-02-20 16:08:07 +0100

    chore: Development environment

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flake of NUI project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "nui";
+
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          clippy
+          rustfmt
+          pkg-config
+        ];
+
+        shellHook = "";
+      };
+    };
+}
