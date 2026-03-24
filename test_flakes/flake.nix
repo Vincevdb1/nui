30a812c17027647461202eb3cf9b5559db004ab7
Vincevdb1 <vincent.vandebosch@icloud.com>
2026-03-24 19:05:34 +0100

    chore: Add more to test flake

--- test_flakes/flake.nix
+++ test_flakes/flake.nix
@@ -7,21 +7,38 @@
   };
 
   outputs =
-    { self, nixpkgs, nixpkgs-stable }:
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "python shell";
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          name = "python shell";
 
-        buildInputs = with pkgs; [
-          python312Full
-          isort
-        ];
+          buildInputs = with pkgs; [
+            python312Full
+            isort
+          ];
 
-        shellHook = "";
+          shellHook = "";
+        };
+
+        another = pkgs.mkShell {
+          name = "another shell";
+
+          buildInputs = with pkgs; [
+            hello
+            testssl
+          ];
+
+          shellHook = "";
+        };
       };
     };
 }
